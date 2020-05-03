c4675c13c3c6f9e95b2b5061b6df3dc05903a037
Venkateswara  P. Raju <VenkateswaraR@in.unibiz.com>
2020-05-03 23:29:32 +0530

    feat: card css updated

--- app/components/Tabs/index.js
+++ app/components/Tabs/index.js
@@ -6,10 +6,23 @@ export default () => (
     <Tabs>
       <TabList>
         <Tab>
-        <h4>Title 2</h4>
-        <i class="fa fa-times" data-unicode="f00d"></i>
+          <h4>Title 1</h4>
+          <button type="button" className="action-btn">
+            <i class="fa fa-times" data-unicode="f00d"></i>
+          </button>
         </Tab>
-        <Tab><h4>Title 1</h4></Tab>
+        <Tab>
+          <h4>Title 2</h4>
+          <button type="button" className="action-btn">
+            <i class="fa fa-times" data-unicode="f00d"></i>
+          </button>
+        </Tab>
+        <Tab>
+          <h4>Title 3</h4>
+          <button type="button" className="action-btn">
+            <i class="fa fa-times" data-unicode="f00d"></i>
+          </button>
+        </Tab>
       </TabList>
       <div className='tab-panel-warper'>
         <TabPanel>
@@ -24,8 +37,15 @@ export default () => (
         </div>
         </TabPanel>
         <TabPanel>
-            <Card></Card>
+          <div className='card-widget full-col-with'>
+              <Card></Card>
+          </div>
         </TabPanel>
+          <TabPanel>
+            <div className='card-widget full-col-with'>
+                <Card></Card>
+            </div>
+          </TabPanel>
       </div>
     </Tabs>
   );
